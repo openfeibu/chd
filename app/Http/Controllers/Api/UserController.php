aadf09afea499459bc51c7fcf467d99ba8ebd30d
wzj <wzj@39cca71d-485a-f04c-a6db-409da0ef9732>
2019-05-24 08:01:24 +0000

    git-svn-id: https://39.108.144.9:8443/svn/chd@51 39cca71d-485a-f04c-a6db-409da0ef9732

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -20,8 +20,7 @@ class UserController extends BaseController
     }
     public function getUser(Request $request)
     {
-        $user = app(User::class)->findUserByToken($request->token);
-
+        $user = User::getUser();
         return response()->json([
             'code' => '200',
             'data' => $user,
@@ -60,18 +59,18 @@ class UserController extends BaseController
     }
     public function submitLocation(Request $request)
     {
-        $user = User::getUser();
+        //$user = User::getUser();
         $longitude = $request->input('longitude','');
         $latitude =  $request->input('latitude','');
         $amap_service = new AmapService();
 
         $data = $amap_service->geocode_regeo($longitude.','.$latitude);
 
-        User::where('id',$user->id)->update([
-            'longitude' => $longitude,
-            'latitude' => $latitude,
-            'city' => $data['regeocode']['addressComponent']['city'],
-        ]);
+//        User::where('id',$user->id)->update([
+//            'longitude' => $longitude,
+//            'latitude' => $latitude,
+//            'city' => $data['regeocode']['addressComponent']['city'],
+//        ]);
 
         return response()->json([
             'code' => '200',
